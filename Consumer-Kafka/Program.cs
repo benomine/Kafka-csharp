742a73efe1c536eddb2edd9dcd164806cf51ef05
Benjamin <benjamin.nomine@outlook.fr>
2021-11-09 20:19:02 -0500

    Ajoutez des fichiers projet.

---
+++ Consumer-Kafka/Program.cs
@@ -0,0 +1,66 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+
+var consumerConfig = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "localhost:9092"
+};
+
+using (var cons = new ConsumerBuilder<Ignore, string>(consumerConfig).Build())
+{
+    cons.Subscribe("test");
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true;
+        cts.Cancel();
+    };
+
+    try
+    {
+        while (true)
+            try
+            {
+                var cr = cons.Consume(cts.Token);
+                var person = JsonConvert.DeserializeObject<Person>(cr.Message.Value);
+                Console.WriteLine(person.ToString());
+                foreach(var note in person.Notes)
+                {
+                    Console.WriteLine($"{note.Text} {note.Created}");
+                }
+                Console.WriteLine(person.GetType());
+                Console.WriteLine("---");
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine(e);
+            }
+    }
+    catch (Exception)
+    {
+        cons.Close();
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string LastName { get; set; }
+    public IEnumerable<Note>? Notes { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {LastName} is {Age} years old";
+    }
+}
+
+public class Note
+{
+    public string? Text { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+}
+
---
+++ Producer-Kafka/Program.cs
@@ -0,0 +1,62 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+var person = new Person
+{
+    Name = "Bob",
+    Age = 69,
+    LastName = "Marley",
+    Notes = new List<Note> { new Note
+    {
+        Text = "Lalalalal",
+        Created = DateTime.Now,
+        Updated = DateTime.Now
+    },
+    new Note
+    {
+        Text = "Tsoin Tsoin",
+        Created = DateTime.Now.AddSeconds(3600),
+        Updated = DateTime.Now.AddSeconds(5000)
+    }
+    }
+};
+
+using (var p = new ProducerBuilder<string, string>(producerConfig).Build())
+{
+    while(true) {
+        var message = new Message<string, string>()
+        {
+            Key = null,
+            Value = JsonConvert.SerializeObject(person)
+        };
+        p.ProduceAsync("test", message);
+        Thread.Sleep(5000);
+    }
+}
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string LastName { get; set; }
+    public IEnumerable<Note>? Notes { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {LastName} is {Age} years old";
+    }
+}
+
+public class Note
+{
+    public string? Text { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
+}
+
